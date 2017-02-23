360ae2242248190785a89698fa7d39676bf8ff4f
Humberto Jaimes <humberto.jaimes@outlook.com>
2017-02-22 23:55:56 -0600

    Agregado ejercicio resuelto
    Se agrego el ejercicio que debería quedar como resultado de la
    realización del laboratorio

---
+++ Fin/Contacts.UITests/AppInitializer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace Contacts.UITests
+{
+	public class AppInitializer
+	{
+		public static IApp StartApp(Platform platform)
+		{
+			// TODO: If the iOS or Android app being tested is included in the solution 
+			// then open the Unit Tests window, right click Test Apps, select Add App Project
+			// and select the app projects that should be tested.
+			//
+			// The iOS project should have the Xamarin.TestCloud.Agent NuGet package
+			// installed. To start the Test Cloud Agent the following code should be
+			// added to the FinishedLaunching method of the AppDelegate:
+			//
+			//    #if ENABLE_TEST_CLOUD
+			//    Xamarin.Calabash.Start();
+			//    #endif
+			if (platform == Platform.Android)
+			{
+				return ConfigureApp
+					.Android
+					.InstalledApp("com.hjr.contactsapp")
+					// TODO: Update this path to point to your Android app and uncomment the
+					// code if the app is not included in the solution.
+					//.ApkFile ("../../../Droid/bin/Debug/xamarinforms.apk")
+					.StartApp();
+			}
+
+			return ConfigureApp
+				.iOS
+				.InstalledApp("com.hjr.contactsapp")
+				.DeviceIdentifier("4BE21B94-099B-4C3E-83C1-BEACBACCCDB5")
+				// TODO: Update this path to point to your iOS app and uncomment the
+				// code if the app is not included in the solution.
+				//.AppBundle ("../../../iOS/bin/iPhoneSimulator/Debug/XamarinForms.iOS.app")
+				.StartApp();
+		}
+	}
+}
---
+++ Fin/Contacts.UITests/Tests.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace Contacts.UITests
+{
+	[TestFixture(Platform.Android)]
+	[TestFixture(Platform.iOS)]
+	public class Tests
+	{
+		IApp app;
+		Platform platform;
+
+		public Tests(Platform platform)
+		{
+			this.platform = platform;
+		}
+
+		[SetUp]
+		public void BeforeEachTest()
+		{
+			app = AppInitializer.StartApp(platform);
+		}
+
+		[Test]
+		public void AppLaunches()
+		{
+			app.Screenshot("La app inicio");
+		}
+
+		[Test]
+		public void LoginWillSuccess()
+		{
+			app.Tap(x => x.Marked("entUserName"));
+			app.EnterText(x => x.Marked("entUserName"), "Usuario3");
+			app.Screenshot("Se introdujo un usuario: Usuario3");
+			app.Tap(x => x.Marked("entPassword"));
+			app.EnterText(x => x.Marked("entPassword"), "Password67");
+			app.Screenshot("Se introdujo un password: Password67");
+			app.Tap(x => x.Marked("btnLogin"));
+			app.Screenshot("Se presiono el inicio");
+			app.WaitForNoElement("indIsBusy", "No desaparecio IsBusy");
+			Assert.NotNull(app.Query("Perfil").First(), "No hay elemento perfil");
+		}
+
+		[Test]
+		public void LoginWillFail()
+		{
+			app.EnterText(x => x.Marked("entPassword"), "Password67");
+			app.Screenshot("Se introdujo un password: Password67");
+			app.Tap(x => x.Marked("btnLogin"));
+			app.Screenshot("Se presiono el inicio");
+			app.WaitForNoElement("indIsBusy", "No aparecio error IsBusy");
+			var errorDialog = app.Query("Error");
+			Assert.IsTrue(errorDialog.Any(), "No se mostro el error");
+		}
+
+		[Test]
+		public void CellWillBePressed()
+		{
+			LoginWillSuccess();
+			app.Tap(x => x.Marked("entBirthday"));
+			app.Screenshot("Tapped on view with class: EntryEditText marked: entBirthday");
+			app.EnterText(x => x.Marked("entBirthday"), "12/12/1990");
+			app.Tap(x => x.Marked("btnSaveProfile"));
+			app.Screenshot("Se guardo el perfil");
+			app.WaitForNoElement("indIsBusy", "No aparecio error IsBusy");
+			if (platform == Platform.Android)
+				app.TouchAndHold(x => x.Index(4));
+			else
+				app.SwipeRightToLeft(x => x.Index(4));
+			app.Screenshot("Se hizo un long press en una celda");
+			var deleteOption = app.Query("Eliminar");
+			Assert.GreaterOrEqual(deleteOption.Count(), 1, "No hay opcion de eliminar");
+		}
+
+	}
+}
